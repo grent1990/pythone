c6c3b8d7486ac0baa09beee85e7e5fbe66b3fb49
Grent <grentsota@gmail.com>
2025-01-27 10:49:34 +0100

    Delete media.py

--- media.py
+++
@@ -1,9 +0,0 @@
-contatore = 0
-numero = input("Inserisci un numero: ")
-somma = 0
-
-while numero != "fine":
-    contatore += 1
-    somma += float(numero)
-    numero = input("Inserisci un altro numero: ")
-print ("La tua media è: ", somma/contatore)
