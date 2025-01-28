2dcce0341cf97ab94565962a979b38ebf9baea46
Grent <grentsota@gmail.com>
2025-01-28 13:35:07 +0100

    Delete statistica.py

--- statistica.py
+++
@@ -1,8 +0,0 @@
-contenitore = []
-
-for voto in range(10):
-    valutazione = input("Inserisci la tua valutazione che va da 1 a 5: ")
-    contenitore.append(int(valutazione))
-contenitore.remove(min(contenitore))
-print(contenitore)
-print("La tua media è: ", sum(contenitore)/len(contenitore))
